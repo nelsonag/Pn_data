7019e885525fe1d2a8c023a2e8e4df10b232f543
Adam Nelson <nelsonag@umich.edu>
2015-05-02 13:03:58 -0400

    Updating for a short run to create a sample data set.

--- pn.py
+++ pn.py
@@ -6,7 +6,7 @@ import scipy.interpolate as sint
 from statepoint import StatePoint
 from matplotlib import pyplot as plt
 from uncertainties import ufloat
-from gen_mgxs import mgxs, dlN, dlA, dlA_momwgt
+from gen_mgxs import mgxs
 import pickle
 from bisect import bisect
 import os
@@ -67,7 +67,7 @@ def get_openmc_mesh(spFile, tid, sid, G, N, extent):
 
     keff = ufloat(sp.k_combined[0], sp.k_combined[1])
 
-    GN = [[ufloat(0,0) for n in xrange(N)] for g in xrange(G)]
+    GN = [[0.0 for n in xrange(N)] for g in xrange(G)]
     data = np.array(GN[:][:])
     dx = extent / float(N)
     x = [(float(i) + 0.5) * dx for i in xrange(N)]
@@ -75,11 +75,9 @@ def get_openmc_mesh(spFile, tid, sid, G, N, extent):
         myg = G - g - 1
         for n in xrange(N):
             m, u = sp.get_value(tid, [('mesh',(1,1,n+1)),('energyin',g)], sid)
-            data[myg][n] = ufloat(m,u)
-        myint = np.trapz(data[myg][:],x)
-        data[myg][:] /= myint
+            data[myg,n] = m
 
-    return x, data, keff
+    return x, data[:,:], keff
 
 def get_openmc_mesh_matrix(spFile, tid, sid, G, N, extent):
     sp = StatePoint(spFile)
@@ -88,8 +86,7 @@ def get_openmc_mesh_matrix(spFile, tid, sid, G, N, extent):
 
     keff = ufloat(sp.k_combined[0], sp.k_combined[1])
 
-    GN = [[ufloat(0,0) for n in xrange(N)] for g in xrange(G)]
-    GGN = [[[ufloat(0,0) for n in xrange(N)] for g in xrange(G)] for g in xrange(G)]
+    GGN = [[[0.0 for n in xrange(N)] for go in xrange(G)] for g in xrange(G)]
     data = np.array(GGN[:][:][:])
     dx = extent / float(N)
     x = [(float(i) + 0.5) * dx for i in xrange(N)]
@@ -100,9 +97,9 @@ def get_openmc_mesh_matrix(spFile, tid, sid, G, N, extent):
             for n in xrange(N):
                 m, u = sp.get_value(tid, [('mesh',(1,1,n+1)),('energyin',g),
                                     ('energyout',go)], sid)
-                data[myg][mygo][n] = ufloat(m,u)
+                data[myg,mygo,n] = m
 
-    return x, data, keff
+    return x, data[:,:,:], keff
 
 def get_omc_mgxs(sp, mesh_tids, mesh_sids, order, G, Nmesh, extent, xstype):
     # Get flux-yN
@@ -133,46 +130,66 @@ def get_omc_mgxs(sp, mesh_tids, mesh_sids, order, G, Nmesh, extent, xstype):
         sid = mesh_sids[3][l]
         x, totalyn[l], omck = get_openmc_mesh(sp,tid,sid,G,Nmesh,extent)
 
-    totaliso = totalyn[0][:][:]
+    # Get nu-fission (right now only doing iso weighting)
+    nusigfns = [None for l in range(order)]
+    tid = mesh_tids[4]
+    sid = mesh_sids[4][0]
+    # Now only doing iso weighting so l=0
+    x, nusigfns[0], omck = get_openmc_mesh_matrix(sp,tid,sid,G,Nmesh,extent)
+    for l in range(1,order):
+        nusigfns[l] = np.zeros_like(nusigfns[0])
+
+
+    Qns = [np.asarray([[0.0 for n in xrange(Nmesh)] for go in xrange(G)]) for l in xrange(order)]
+    # put Q in nusigfns, leave as isotropic now
+    l = 0
+    for go in range(G):
+        for n in range(Nmesh):
+            Qns[l][go,n] = 0.0
+            for g in range(G):
+                Qns[l][go,n] += nusigfns[0][g,go,n]
+    for l in range(1,order):
+        for g in range(G):
+            for n in range(Nmesh):
+                Qns[l][g,n] = 0.0
+
+    totaliso = totalyn[0][:,:]
     for l in range(order):
         for g in range(G):
             for n in range(Nmesh):
                 # Nmeshormalize by flux
-                flux = fluxyn[l][g][n]
-                flux0 = fluxyn[0][g][n]
+                flux = fluxyn[l][g,n]
+                flux0 = fluxyn[0][g,n]
                 if flux0 != 0.0:
                     for go in range(G):
-                        scattpn[l][g][go][n] /= flux0
+                        scattpn[l][g,go,n] /= flux0
                     if l == 0:
-                        totaliso[g][n] /= flux0
+                        totaliso[g,n] /= flux0
                 if flux != 0.0:
                     for go in range(G):
-                        scattyn[l][g][go][n] /= flux
-                    totalyn[l][g][n] /= flux
+                        scattyn[l][g,go,n] /= flux
+                        nusigfns[l][g,go,n] /= flux
+                    totalyn[l][g,n] /= flux
+
                 # Apply correction
                 if xstype == 'consP':
-                    corr = totaliso[g][n] - totalyn[l][g][n]
+                    corr = totaliso[g,n] - totalyn[l][g,n]
                     for go in range(G):
-                        scattyn[l][g][go][n] += corr
+                        scattyn[l][g,go,n] += corr
 
     if xstype == 'iso':
-        sigtns = [totaliso[:][:] for l in range(order)]
-        sigsns = scattpn[:][:][:][:]
+        sigtns = [totaliso for l in range(order)]
+        sigsns = scattpn[:]
     elif xstype == 'consP':
-        sigtns = [totaliso[:][:] for l in range(order)]
-        sigsns = scattyn[:][:][:][:]
+        sigtns = [totaliso for l in range(order)]
+        sigsns = scattyn[:]
     elif xstype == 'yN':
-        sigtns = totalyn[:][:][:]
-        sigsns = scattyn[:][:][:][:]
+        sigtns = totalyn[:]
+        sigsns = scattyn[:]
 
-    return omck, x, sigtns, sigsns  # Eventually need source from here too
+    print totaliso[0]
 
-def get_1gdata(sigtns, sigsns, G, N, Nmesh):
-    for n in range(N+1):
-        for g in range(G):
-            for ix in range(Nmesh):
-                for go in range(G):
-                    pass
+    return omck, x, sigtns, sigsns, nusigfns, Qns
 
 
 if __name__ == "__main__":
@@ -181,46 +198,46 @@ if __name__ == "__main__":
                  'font.size': 16}
     plt.rcParams.update(newparams)
 
-    if len(sys.argv) != 2:
-        raise ValueError("Must Provide Run Type [consP, iso, yN]!")
+    if len(sys.argv) != 3:
+        raise ValueError("Must Provide Cross-Section Type [consP, iso, yN] & " +
+                         "Run Type [FS, k]!")
     else:
         xstype = sys.argv[1]
         if xstype not in ["consP", "iso", "yN"]:
-            raise ValueError("Invalid Type!")
+            raise ValueError("Invalid Cross-Section Type!")
+        runtype = sys.argv[2]
+        if runtype not in ["FS", "k"]:
+            raise ValueError("Invalid Run Type!")
+
 
     show = False
     save = True
     G = 4
     N = 3
-    Nmesh = 200
-    extent = 10.0
+    Nmesh = 16
+    extent = 0.64
     sp = './statepoint.10000.binary'
 
     # First get the mgxs data and create x/s
     if xstype == 'iso':
-        dL = dlA
         momWgt = False
         trcorr = None
     elif xstype == 'consP':
-        dL = dlA_momwgt
         momWgt = True
         trcorr = 'consP'
     elif xstype == 'yN':
-        dL = dlA_momwgt
         momWgt = True
         trcorr = None
 
-    data = mgxs(sp, dL, 'cell', momWgt, trcorr)
-    data.print_MPACT('data.xs', xstype)
-    mesh_tids = [4, 5, 5, 6]
-    mesh_sids = [[1,3,7,13], [0,1,2,3], [4,6,10,14], [0,2,6,12]]
+    mesh_tids = [0, 1, 1, 0, 2]
+    mesh_sids = [[0,2,6,12], [0,1,2,3], [4,6,10,16], [16,18,22,27], [0]]
 
-    omck, x, sigtns, sigsns = get_omc_mgxs(sp, mesh_tids, mesh_sids, N+1, G, Nmesh, extent, xstype)
+    omck, x, sigtns, sigsns, nusigfns, Qns = get_omc_mgxs(sp, mesh_tids,
+                                                          mesh_sids, N+1, G,
+                                                          Nmesh, extent, xstype)
     print 'OpenMC k_eff=' + "{:12.5E}".format(omck)
-
-    # Now we condense down to 1g
-    sigtns, sigsns = get_1gdata(sigtns, sigsns)
 
     # Set up some of our data we will use during the sweep
     invdx, n_ratios = init(x, N)
-    print n_ratios
+
+
